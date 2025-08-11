d4ba60bd3c5a1554ebddd5c2004950870f470e68
parkm2ngyu00 <parkm2ngyu00@naver.com>
2025-08-11 16:11:01 +0900

    feat(mfa): implement handle mfa error func

--- api/mfa/mfa.go
+++ api/mfa/mfa.go
@@ -3,7 +3,9 @@ package mfa
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/alpacax/alpacon-cli/api/server"
 	"github.com/alpacax/alpacon-cli/client"
+	"github.com/alpacax/alpacon-cli/config"
 	"github.com/alpacax/alpacon-cli/utils"
 )
 
@@ -13,6 +15,27 @@ const (
 
 type mfaResponse struct {
 	MfaURL string `json:"mfa_url"`
+}
+
+func HandleMFAError(ac *client.AlpaconClient, serverName string) error {
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		utils.CliError("Failed to load configuration: %s.", err)
+	}
+
+	serverID, _ := server.GetServerIDByName(ac, serverName)
+	mfaURL, err := GetMFALink(ac, serverID, cfg.WorkspaceName)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\n==================== AUTHENTICATION REQUIRED ====================")
+	fmt.Println("\nPlease authenticate by visiting the following URL:")
+	fmt.Printf("%s\n\n", mfaURL)
+	fmt.Print("===============================================================\n\n")
+
+	return nil
 }
 
 func GetMFALink(ac *client.AlpaconClient, serverID string, workspaceName string) (string, error) {
