39b2287d727e8778502e0837e69d389491aacea0
Jisung Chae <jschae02@khu.ac.kr>
2026-04-23 15:54:13 +0900

    feat(token): add scopes fields and scope types to auth types

--- api/auth/types.go
+++ api/auth/types.go
@@ -14,8 +14,9 @@ type LoginResponse struct {
 }
 
 type APITokenRequest struct {
-	Name      string  `json:"name"`
-	ExpiresAt *string `json:"expires_at"`
+	Name      string   `json:"name"`
+	ExpiresAt *string  `json:"expires_at"`
+	Scopes    []string `json:"scopes,omitempty"`
 }
 
 type APITokenDuplicateRequest struct {
@@ -29,6 +30,7 @@ type APITokenResponse struct {
 	Key       string    `json:"key"`
 	UpdatedAt time.Time `json:"updated_at"`
 	ExpiresAt time.Time `json:"expires_at"`
+	Scopes    []string  `json:"scopes"`
 }
 
 type APITokenAttributes struct {
@@ -37,4 +39,22 @@ type APITokenAttributes struct {
 	Enabled   bool   `json:"enabled"`
 	UpdatedAt string `json:"updated_at" table:"Updated At"`
 	ExpiresAt string `json:"expires_at" table:"Expires At"`
+	Scopes    string `json:"scopes" table:"Scopes"`
+}
+
+type TokenScopesResponse struct {
+	Resources []TokenScopeResource `json:"resources"`
+	Wildcards []string             `json:"wildcards"`
+}
+
+type TokenScopeResource struct {
+	Name    string   `json:"name"`
+	Actions []string `json:"actions"`
+	ACL     []string `json:"acl"`
+}
+
+type TokenScopeAttributes struct {
+	Resource string `json:"name" table:"Resource"`
+	Actions  string `json:"actions" table:"Actions"`
+	ACL      string `json:"acl" table:"ACL"`
 }
