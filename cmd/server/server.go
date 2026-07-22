8d2aec82ec0f24c299496cf75f58fbacd8c0c110
Jisung Chae <js.chae@alpacax.com>
2026-07-22 09:52:48 +0900

    feat(cmd): add server refresh command and list system actions in usage

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -15,7 +15,7 @@ var ServerCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-		return errors.New("a subcommand is required. Use 'alpacon server list', 'alpacon server create', 'alpacon server describe', 'alpacon server update', 'alpacon server delete', or 'alpacon server token'. Run 'alpacon server --help' for more information")
+		return errors.New("a subcommand is required. Use 'alpacon server list', 'alpacon server create', 'alpacon server describe', 'alpacon server update', 'alpacon server delete', 'alpacon server reboot', 'alpacon server shutdown', 'alpacon server upgrade', 'alpacon server refresh', or 'alpacon server token'. Run 'alpacon server --help' for more information")
 	},
 }
 
---
+++ cmd/server/server_refresh.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"github.com/alpacax/alpacon-cli/api/server"
+	"github.com/alpacax/alpacon-cli/client"
+	"github.com/alpacax/alpacon-cli/utils"
+	"github.com/spf13/cobra"
+)
+
+var refreshServerCmd = &cobra.Command{
+	Use:     "refresh SERVER",
+	Short:   "Refresh a server's system information",
+	Example: `alpacon server refresh myserver`,
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		serverName := args[0]
+
+		ac, err := client.NewAlpaconAPIClient()
+		if err != nil {
+			utils.CliErrorWithExit("Connection to Alpacon API failed: %s. Consider re-logging.", err)
+		}
+
+		err = server.RequestServerAction(ac, serverName, "update_information", false)
+		if err != nil {
+			utils.CliErrorWithExit("Failed to refresh the server information: %s.", err)
+		}
+
+		utils.CliSuccess("System information refresh requested. Run 'alpacon events' to monitor progress.")
+	},
+}
+
+func init() {
+	ServerCmd.AddCommand(refreshServerCmd)
+}
